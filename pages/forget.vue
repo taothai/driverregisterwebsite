524769914b70f7577b0d3bead2ac308a708b8c3d
jshow101 <jshow101@gmail.com>
2021-08-20 15:07:22 +0700

    ;list

---
+++ pages/forget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="Mainpage">
+      <iframe src="https://gecc.dlt.go.th:4447/2020-12-29/web_booking/page/pagerepassword.php" title="description" width="100%" height="100%"></iframe>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped>
+.Mainpage{
+    height: 105vh;
+    width: 106vw;
+    margin-left: -15px;
+    margin-top: -10px;
+    overflow: hidden;
+    background-color:#6f81ca ;
+    
+
+}
+._myiframe{
+  
+  width: 100%;
+  border:none;
+  overflow-x: hidden;
+  
+}
+</style>
---
+++ pages/train.vue
@@ -0,0 +1,46 @@
+<template class ="PageCenter">
+    <div class="Mainpage">
+
+   <a href="https://www.dlt-elearning.com/Home">   
+      <div class="button">
+         เข้าอบรมออนไลน์
+       </div>
+  </a> 
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped>
+.Mainpage{
+    height: 100vh;
+    width: 100%;
+    background: rgb(129,77,123);
+    background: linear-gradient(176deg, rgba(129,77,123,1) 36%, rgba(100,72,131,1) 100%);
+     display: flex;
+    justify-content: center;
+    align-items: center;
+
+    
+
+}
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+._myiframe{
+  
+  width: 100%;
+  border:none;
+  overflow-x: hidden;
+  
+}
+
+</style>
